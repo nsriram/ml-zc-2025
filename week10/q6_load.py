0a41cc8ac0040955944d0e47e2bd82426a6faa41
N Sriram <sriramn@thoughtworks.com>
2026-01-09 14:53:01 +0530

    Week 10 homework completion

---
+++ week10/q6_load.py
@@ -0,0 +1,10 @@
+import requests
+from time import sleep
+
+url = "http://localhost:9696/predict"
+
+client = {"job": "management", "duration": 400, "poutcome": "success"}
+while True:
+    sleep(0.05)
+    response = requests.post(url, json=client).json()
+    print(response)
